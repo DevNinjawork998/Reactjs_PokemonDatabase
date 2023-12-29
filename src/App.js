8912af8495a87031445cd9ecc9869b3f3775ff4a
Jack Ooi <jack.ooi@bp.com>
2023-12-29 10:39:51 +0800

    Add pokemon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import './App.css';
+import React from 'react';
+import Pokemon from './Pokemon';
+
+
 
 function App() {
   return (
-    <div></div>
+    <div style={{
+      margin: "auto",
+      width: 800,
+      paddingTop: "1rem",
+    }}>
+      <h1 className='title'>Pokemon Search</h1>
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Pokemon.slice(0, 20).map((pokemon) => (
+            <tr
+              key={pokemon.id}
+            >
+              <td>{pokemon.name.english}</td>
+              <td>{pokemon.type.join(',')}</td>
+            </tr>))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
