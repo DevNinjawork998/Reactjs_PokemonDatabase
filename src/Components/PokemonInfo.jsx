d7a73d2323799c05134ebd85a04108639a03ab2b
jack.ooi <jack.ooi@bp.com>
2024-01-04 21:29:11 +0800

    Updating Table tag using Bootstrap

--- src/Components/PokemonInfo.jsx
+++ src/Components/PokemonInfo.jsx
@@ -1,26 +1,25 @@
 import React, { useContext } from "react";
-import styled from "@emotion/styled";
-
+import Table from "react-bootstrap/Table";
 import PokemonContext from "../PokemonContext";
 
 const PokemonInfo = () => {
-    const { selectedPokemon } = useContext(PokemonContext);
-  
-    return selectedPokemon ? (
-      <div>
-        <h2>{selectedPokemon.name.english}</h2>
-        <table>
-          <tbody>
-            {Object.keys(selectedPokemon.base).map((key) => (
-              <tr key={key}>
-                <td>{key}</td>
-                <td>{selectedPokemon.base[key]}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    ) : null;
-  };
+  const { selectedPokemon } = useContext(PokemonContext);
 
-  export default PokemonInfo;
+  return selectedPokemon ? (
+    <div>
+      <h2>{selectedPokemon.name.english}</h2>
+      <Table>
+        <tbody>
+          {Object.keys(selectedPokemon.base).map((key) => (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>{selectedPokemon.base[key]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  ) : null;
+};
+
+export default PokemonInfo;
