8e26ae314fe531b115fa6e8c4bb9cbf3af6b5baa
jack.ooi <jack.ooi@bp.com>
2024-01-05 17:15:49 +0800

    modified:   src/Components/PokemonRow.jsx

--- src/Components/PokemonRow.jsx
+++ src/Components/PokemonRow.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Button } from "react-bootstrap";
-import styled from "@emotion/styled";
 
 import PokemonType from "../PokemonType";
 
